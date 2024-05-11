993d57f08f2240c18d35550adf6b652727c44425
rono516 <ronocollins2000@gmail.com>
2024-05-11 14:52:35 +0300

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>A site for events to better the world.</h1>
+  </div>
+</template>
+
+<style>
+/* @media (min-width: 1024px) {
+  .about {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+} */
+</style>
---
+++ src/views/EventListView.vue
@@ -0,0 +1,45 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+const events = ref([
+  {
+    id: 5928101,
+    category: 'animal welfare',
+    title: 'cat adoption day',
+    description: 'find your new feline friend at this event',
+    location: 'meow town',
+    date: 'January 28 2024',
+    time: '12:00',
+    petsAllowed: true,
+    organizer: 'Kat Laydee'
+  },
+  {
+    id: 5928102,
+    category: 'animal welfare',
+    title: 'cat adoption day',
+    description: 'find your new feline friend at this event',
+    location: 'meow town',
+    date: 'January 29 2024',
+    time: '13:00',
+    petsAllowed: true,
+    organizer: 'Kat Laydee'
+  }
+])
+</script>
+
+<template>
+
+  <div class="events"><EventCard v-for="event in events" :key="event.id" :event="event" /></div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  font-size: 20px;
+}
+</style>
